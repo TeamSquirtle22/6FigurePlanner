52e3ac9dff43b988fd077964a726dfe0d2e06271
Eric  Marcatoma <ericmarcatoma@new-host-7.home>
2021-01-11 17:04:50 -0500

    webpack

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send('Internal Server Error');
+});
+
+//catch all
+app.use('*', (req, res) => {
+  return res.status(404).send('invalid end point');
+});
+
+// //global error handler
+app.use(function (err, req, res, next) {
+  const defaultError = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500, //always go 500 with server side errors
+    message: {err: 'An error occurred'},
+  };
+  const errorObj = Object.assign(defaultError, err);
+  console.log(errorObj.log);
+  res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!');
+});
+
+module.exports = app;
