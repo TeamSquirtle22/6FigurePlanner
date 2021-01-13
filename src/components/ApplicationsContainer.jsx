f30feed581c78d39e7bab107369dab3b33914002
Kristiina Eelnurme <kristiina.eelnurme@gmail.com>
2021-01-13 12:08:14 -0500

    got form/drawer component setup and fixed component hierarchy

--- src/components/ApplicationsContainer.jsx
+++ src/components/ApplicationsContainer.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import { Container } from '@chakra-ui/react';
 import ApplicationsTable from './ApplicationsTable.jsx';
+import AddApplicationForm from './AddApplicationForm.jsx';
+
+//This container holds all state bc dashboard and form need to share this state
 
 function ApplicationsContainer() {
+	
+  // state for signin (id, username)
+  // pass that in as props to applications table and form using setter funcs or values
+
+  // store state for form 
+  // store state for dashboard/table
+
+  // const initialValues = {
+// 	company: '',
+//   position: '',
+//   companyEmail: '',
+//   companyPhone: '',
+// 	dateApplied: '',
+// 	interviewStatus: '',
+// 	offer: false,
+// 	doubleDown: '',
+// };
+
+
 	return (
 		<div>
 			<Container
@@ -17,6 +39,7 @@ function ApplicationsContainer() {
 				<h1>Job Application Dashboard</h1>
 				<ApplicationsTable />
 			</Container>
+        <AddApplicationForm />
 		</div>
 	);
 }
