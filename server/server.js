513967f6291e230b2831cf53f2d56ee7e8c095b4
chrisak47 <72534276+chrisak47@users.noreply.github.com>
2021-01-13 15:44:10 -0500

    started OAuth

--- server/server.js
+++ server/server.js
@@ -1,14 +1,50 @@
+/* General Imports */
 const express = require('express');
 const app = express();
 const path = require('path');
+
+/* OAuth */
+const session = require('express-session');
+const passport = require('passport');
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+
+/* Controllers */
 const userController = require('./controller/userController');
 const appController = require('./controller/appController');
 const interviewController = require('./controller/interviewController');
 
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
+
+/* Set Session information to server */
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
+passport.use(new LinkedInStrategy({
+  clientID: config.linkedinAuth.clientID,
+  clientSecret: config.linkedinAuth.clientSecret,
+  callbackURL: config.linkedinAuth.callbackURL,
+  scope: ['r_emailaddress', 'r_liteprofile'],
+}, function (token, tokenSecret, profile, done) {
+  return done(null, profile);
+}
+));
+
 app.get('/', (req, res) => {
   return res.sendFile(path.join(__dirname, '../index.html'));
 });
